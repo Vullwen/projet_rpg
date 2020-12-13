e6c5854677d08ab0812ffa27eeaac371f037073e
smi4th <75613925+smi4th@users.noreply.github.com>
2020-12-13 11:09:28 +0100

    Delete Affichage du jeu.py

--- Affichage du jeu.py
+++
@@ -1,8 +0,0 @@
-from robot import *
-
-cree_fenetre(500,280)
-
-for x in range(20,500,40):
-    for y in range(10, 280, 20):
-        image(x, y, "images/sols_dongeon 2.png",ancrage="center", tag="")
-clic()
